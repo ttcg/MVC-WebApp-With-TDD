302d90b6228aa52f26abb682271f3a42217d3f20
Thet Wai <thetwai2000@gmail.com>
2017-04-17 18:51:04 +0100

    Test Fixture added

---
+++ MVC_WebApp_With_TDD.Tests/StudentsServiceTestFixture.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Moq;
+using MVC_WebApp_With_TDD.Services;
+using MVC_WebApp_With_TDD.Models;
+using System.Data.Entity;
+
+namespace MVC_WebApp_With_TDD.Tests
+{
+    public class StudentsServiceTestFixture 
+    {
+        public IStudentsService StudentService;
+        public Mock<DbContexts.MVCWebAppDbContext> MVCWebAppDbContext;
+        //public List<Student> Students { get; private set; }
+
+        public StudentsServiceTestFixture()
+        {
+            MVCWebAppDbContext = new Mock<DbContexts.MVCWebAppDbContext>();
+
+            StudentService = new StudentsService(MVCWebAppDbContext.Object);
+
+            var campus = new Campus() { CampusID = 1, CampusName = "SMF" };
+            var Students = new List<Student>()
+            {
+                new Student() { StudentID = 1, RefNo = "12456343", FirstName = "John", LastName = "Smith", DateOfBirth = DateTime.Now.AddYears(-10), DateCreated = DateTime.Now, CampusID = 1, Campus = campus },
+                new Student() { StudentID = 2, RefNo = "87984564", FirstName = "Pete", LastName = "Luck", DateOfBirth = DateTime.Now.AddYears(-20), DateCreated = DateTime.Now.AddDays(1), CampusID = 2, Campus = campus }
+            };
+
+            // set up student entity.
+            var mockSet = new Mock<DbSet<Student>>();
+            mockSet.As<IQueryable<Student>>().Setup(m => m.Provider).Returns(Students.AsQueryable().Provider);
+            mockSet.As<IQueryable<Student>>().Setup(m => m.Expression).Returns(Students.AsQueryable().Expression);
+            mockSet.As<IQueryable<Student>>().Setup(m => m.ElementType).Returns(Students.AsQueryable().ElementType);
+            mockSet.As<IQueryable<Student>>().Setup(m => m.GetEnumerator()).Returns(Students.AsQueryable().GetEnumerator());
+
+            MVCWebAppDbContext.Setup(c => c.Students).Returns(mockSet.Object);            
+
+            //MVCWebAppDbContext.Setup(m => m.Students.Include("Campus")).Returns(mockSet.Object);
+        }      
+    }
+}
---
+++ MVC_WebApp_With_TDD.Tests/StudentsServiceTestWithFixture.cs
@@ -0,0 +1,98 @@
+﻿using Moq;
+using MVC_WebApp_With_TDD.Models;
+using MVC_WebApp_With_TDD.Services;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MVC_WebApp_With_TDD.Tests
+{
+    public class StudentsServiceTestWithFixture : IClassFixture<StudentsServiceTestFixture>
+    {
+        private IStudentsService _studentService;
+        private Mock<DbContexts.MVCWebAppDbContext> _dbContext;
+        private List<Student> _students;
+
+        public StudentsServiceTestWithFixture(StudentsServiceTestFixture fixture)
+        {
+            _dbContext = fixture.MVCWebAppDbContext;
+
+            _studentService = fixture.StudentService;
+
+            //_students = fixture.Students;
+        }
+
+        [Fact]
+        public void should_Add_Student()
+        {
+            var student = new Student() { StudentID = 3, RefNo = "12456343", FirstName = "John", LastName = "Smith", DateOfBirth = DateTime.Now.AddYears(-10) };
+
+            _studentService.Insert(student);
+
+            Assert.True(student.StudentID > 0);
+        }
+
+        [Fact]
+        public void GetAll()
+        {
+            var result = _studentService.GetAll();            
+
+            Assert.Equal(2, result.Count());
+            Assert.Equal(2, result.FirstOrDefault().StudentID);
+
+            _dbContext.Verify(v => v.Students.Include("Campus"), Times.Once);
+        }
+
+        [Fact]
+        public void GetDetail()
+        {
+            var result = _studentService.GetDetail(1);
+
+            Assert.Equal(1, result.StudentID);
+        }
+
+        [Fact]
+        public void Update()
+        {
+            var student = new Student() { StudentID = 1, RefNo = "12456343", FirstName = "John", LastName = "Smith", DateOfBirth = DateTime.Now.AddYears(-10) };
+
+            _studentService.Update(student);
+
+            Assert.True(student.StudentID > 0);
+
+            _dbContext.VerifyAll();
+        }
+
+        //[Fact]
+        //public void Delete()
+        //{            
+        //    int idToDelete = 2;            
+
+        //    // call delete method now
+        //    _studentService.Delete(idToDelete);
+
+        //    _dbContext.Setup(m => m.Students.Remove(It.IsAny<Student>())).Callback<Student>((entity) => _students.Remove(entity));
+
+        //    _dbContext.Setup(s => s.Students.Find(idToDelete)).Returns(_students.Single(s => s.StudentID == idToDelete));
+
+        //    // 1 object deleted, it should return 1
+        //    Assert.Equal(1, _students.Count());  // <----- Error here
+
+        //    _dbContext.Verify(s => s.Students.Find(idToDelete), Times.Once);
+        //    _dbContext.Verify(s => s.Students.Remove(It.IsAny<Student>()), Times.Once);
+        //    _dbContext.Verify(s => s.SaveChanges(), Times.Once);
+        //}
+
+        [Fact]
+        public void GetByName()
+        {
+            var result = _studentService.GetByName("John", "Smith");
+
+            Assert.Equal(1, result.StudentID);
+        }
+    }
+}
